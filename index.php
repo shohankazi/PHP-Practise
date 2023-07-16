aa16d02235088a12aa04da0d344d01bb0e436f9a
shohankazi <shohankazi012@gmail.com>
2023-07-16 23:10:35 +0600

    operator concept cleared

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-    echo "Hello world\n";
-// Defining Constant
-    define('PI', 3.1416);
-    echo PI
+    echo "Hello world";
+    echo '</br>';
+
+    // define('PI', 3.1416); // Defining Constant
+    // echo PI
+    $number1 = 100;
+    $number2 = 20;
+    echo $number1 + $number2;
+
+
+    // Basic Operators
+    echo '</br>';
+    $name = "SHOHAN";
+    $name .= " KAZI";
+    echo $name;
+    echo '</br>';
+    var_dump(10>5);
 ?>
