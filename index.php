2b4f2d63c22f30b5d9080b77e7b803d2cffccefd
shohankazi <shohankazi012@gmail.com>
2023-07-16 23:17:06 +0600

    if else condition cleared

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
-    echo "Hello world";
-    echo '</br>';
+    // echo "Hello world";
+    // echo '</br>';
 
-    // define('PI', 3.1416); // Defining Constant
-    // echo PI
-    $number1 = 100;
-    $number2 = 20;
-    echo $number1 + $number2;
+    // // define('PI', 3.1416); // Defining Constant
+    // // echo PI
+    // $number1 = 100;
+    // $number2 = 20;
+    // echo $number1 + $number2;
 
 
-    // Basic Operators
-    echo '</br>';
-    $name = "SHOHAN";
-    $name .= " KAZI";
-    echo $name;
-    echo '</br>';
-    var_dump(10>5);
+    // // Basic Operators
+    // echo '</br>';
+    // $name = "SHOHAN";
+    // $name .= " KAZI";
+    // echo $name;
+    // echo '</br>';
+    // var_dump(10>5);
+    $day = 'Sunday';
+    if ($day === 'Friday' || $day === 'Sunday') {
+        echo "We can go on a tour today";
+    } else {
+        echo "You can't go anywhere";
+    }
+    
 ?>
