93c25684beee310362cac844c190b9f96ed6a96e
shohankazi <shohankazi012@gmail.com>
2023-07-16 23:22:19 +0600

    array concept cleared

--- index.php
+++ index.php
@@ -16,11 +16,17 @@
     // echo $name;
     // echo '</br>';
     // var_dump(10>5);
-    $day = 'Sunday';
-    if ($day === 'Friday' || $day === 'Sunday') {
-        echo "We can go on a tour today";
-    } else {
-        echo "You can't go anywhere";
-    }
+
+    // ----- IF ELSE CONDITION -----
     
+    // $day = 'Sunday';
+    // if ($day === 'Friday' || $day === 'Sunday') {
+    //     echo "We can go on a tour today";
+    // } else {
+    //     echo "You can't go anywhere";
+    // }
+    
+    // ------ ARRAY DATA TYPE ------
+    $array = ['Shohan','Kazi',34,35,36];
+    echo $array[3];
 ?>
