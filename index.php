ac0c0e870a0a322dfc147964781a661c227b8c73
shohankazi <shohankazi012@gmail.com>
2023-07-17 12:57:38 +0600

    function concept cleared in PHP

--- index.php
+++ index.php
@@ -92,5 +92,11 @@
     //         echo $value . ' x 5 = ' . $value * 5 . '</br>';
     //     }
 
+    // -------- Function ------
+    function sum($num1,$num2) {
+        return $num1 + $num2;
+    }
+    echo sum(30,40);
+
 ?>
 
